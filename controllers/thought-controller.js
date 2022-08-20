0011a4749faed43b2269a0e00d4f8e29ca46cf01
kaylab78 <kaylabackus78@gmail.com>
2022-08-20 16:50:20 -0500

    Added routes for reactions.

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -58,6 +58,34 @@ const thoughtController = {
                 res.json(dbThoughtData);
             })
             .catch((err) => res.status(400).json(err));
+    },
+
+    // add reaction to thought
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.reactionId },
+            { $push: {reactions: body } },
+            { new: true, runValidators: true }
+        )
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id.' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // remove reaction
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.reactionId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.json(err));
     }
 };
 
