576c1ec75af0b9daf7fb77c6b1e9a37c391e2faf
kaylab78 <kaylabackus78@gmail.com>
2022-08-20 16:40:29 -0500

    Edited Thought.js moving Reaction schema above Thought schema.

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,36 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // getter to format date
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+    }
+
+);
+
 const ThoughtSchema = new Schema(
     {
         thoughtText: {
@@ -20,42 +50,20 @@ const ThoughtSchema = new Schema(
             type: String,
             required: true
         },
-        // reactions: [ReactionSchema]
+        reactions: [ReactionSchema]
     },
     {
         toJSON: {
+            virtual: true,
             getters: true
         },
         id: false
     }
 );
 
-// const ReactionSchema = new Schema(
-//     {
-//         reactionId: {
-//             type: Schema.Types.ObjectId,
-//             default: () => new Types.ObjectId()
-//         },
-//         reactionBody: {
-//             type: String,
-//             required: true,
-//             maxLength: 280
-//         },
-//         username: {
-//             type: String,
-//             required: true
-//         },
-//         createdAt: {
-//             type: DataTransfer,
-//             default: Date.now,
-//             // getter to format date
-//         }
-//     }
-// )
-
-// ThoughtSchema.virtual('reactionCount').get(function() {
-//     return this.reactions.length;
-// });
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
 
 const Thought = model('Thought', ThoughtSchema);
 
